dd58ed9c36db7631ceee6cb22055ad79d282e21a
caiorodrigues9 <caiorodrigues9@gmail.com>
2021-09-05 21:56:33 -0300

    Correção do teste da aplicação

--- tests/Unit/Aplicacao/Aluno/MatricularAlunoTest.php
+++ tests/Unit/Aplicacao/Aluno/MatricularAlunoTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Caio\Arquitetura\Tests\Aplicacao\Aluno;
+namespace Caio\Arquitetura\Tests\Unit\Aplicacao\Aluno;
 
 use Caio\Arquitetura\Aplicacao\Aluno\MatricularAluno\MatricularAluno;
 use Caio\Arquitetura\Aplicacao\Aluno\MatricularAluno\MatricularAlunoDto;
@@ -13,7 +13,7 @@ class MatricularAlunoTest extends TestCase
     public function testAlunoDeveSerAdicionadoAoRepositorio()
     {
         $repositorioAluno = new RepositorioDeAlunosEmMemoriaAlias();
-        $dadosAluno = new MatricularAlunoDto('Teste','123.456.789-12');
+        $dadosAluno = new MatricularAlunoDto('Teste','123.456.789-12','email@email.com');
         $useCase = new MatricularAluno($repositorioAluno);
         $useCase->executa($dadosAluno);
         $aluno = $repositorioAluno->buscarPorCpf(new CPF('123.456.789-12'));
